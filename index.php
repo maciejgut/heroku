e698b56cd5f3f514a1cfe729cd5b95abf68e07d2
maciejgut <33835334+maciejgut@users.noreply.github.com>
2018-08-13 15:29:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
-echo "<h1>Hello World</h1>";
-echo "<p>Hello World from Heroku</p>";
-echo "<p>This is a new paragraph</p>";
+/* Filename: jsonapi.php */    
+/* Declare your variables */      
+$items = array();    
+/* Populate your array with a list of dummy strings */      
+for($i = 0; $i < 10; $i++) {         
+    $items[$i] = "This is message $i";        
+}    
+/* Set the header type for output */
+header('Content-Type: application/json');    
+/* Create a variable to hold your JSON data */  
+ $jsonOutput =  json_encode($items);    
+/* Output the JSON data */       
+ echo $jsonOutput; 
